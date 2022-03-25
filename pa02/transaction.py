14a0904ff40e6950dfab4ffcac32297bee7567d7
Jiangarang <55816618+Jiangarang@users.noreply.github.com>
2022-03-24 20:01:59 -0400

    pylint formatting changes. don't change these

--- pa02/transaction.py
+++ pa02/transaction.py
@@ -1,7 +1,8 @@
 import sqlite3
 
 def to_cat_dict(cat_tuple):
-    cat = {'rowid':cat_tuple[0], 'item#':cat_tuple[1], 'amount':cat_tuple[2], 'category':cat_tuple[3], 'date':cat_tuple[4], 'description':cat_tuple[5]}
+    cat = {'rowid':cat_tuple[0], 'item#':cat_tuple[1], 'amount':cat_tuple[2],
+    'category':cat_tuple[3], 'date':cat_tuple[4], 'description':cat_tuple[5]}
     return cat
 
 def to_cat_dict_list(cat_tuples):
@@ -12,13 +13,10 @@ class Transaction():
     def __init__(self,file_name):
         con= sqlite3.connect(dbfile)
         cur = con.cursor()
-<<<<<<< HEAD
-        cur.execute('''CREATE TABLE IF NOT EXISTS categories
-                    (item# int,amount int,category text,date text,description text)''')
-=======
         cur.execute('''CREATE TABLE IF NOT EXISTS transactions
-                    (item# int,amount int,category text,date date,day text, month text, year text, description text)''')
->>>>>>> 21e02899d019f53ff8926cc9f85080d239282101
+                    (item# int,amount int,category text,
+                    date date,day text, month text, year text, 
+                    description text)''')
         con.commit()
         con.close()
         self.dbfile = dbfile
@@ -26,28 +24,18 @@ class Transaction():
     def show(self):
         con= sqlite3.connect(self.dbfile)
         cur = con.cursor()
-<<<<<<< HEAD
-        cur.execute("select item#,amount,category,date,description from ")
-        toReturn = *cur.fetchall(), sep = "\n"
-        con.commit()
-        con.close()
-        return toReturn
-=======
         cur.execute("SELECT rowid,item#,amount,category,date,description from transactions")
         tuples = cur.fetchall()
         con.commit()
         con.close()
         return to_cat_dict_list(tuples)
->>>>>>> 21e02899d019f53ff8926cc9f85080d239282101
 
     def add(self,item):
         con= sqlite3.connect(self.dbfile)
         cur = con.cursor()
-<<<<<<< HEAD
-        cur.execute("INSERT INTO categories VALUES(?,?,?,?,?)",(item['item#'],item['amount'],item['category'],item['date'],item['description']))
-=======
-        cur.execute("INSERT INTO transactions VALUES(?,?,?,?,?)",(item['item#'],item['amount'],item['category'],item['date'],item['description']))
->>>>>>> 21e02899d019f53ff8926cc9f85080d239282101
+        cur.execute("INSERT INTO transactions VALUES(?,?,?,?,?)",
+        (item['item#'],item['amount'],item['category'],
+        item['date'],item['description']))
         con.commit()
         cur.execute("SELECT last_insert_rowid()")
         last_rowid = cur.fetchone()
@@ -58,11 +46,7 @@ class Transaction():
     def delete(self,rowid):
         con= sqlite3.connect(self.dbfile)
         cur = con.cursor()
-<<<<<<< HEAD
-        cur.execute('''DELETE FROM categories
-=======
         cur.execute('''DELETE FROM transactions
->>>>>>> 21e02899d019f53ff8926cc9f85080d239282101
                        WHERE rowid=(?);
         ''',(rowid,))
         con.commit()
@@ -71,67 +55,39 @@ class Transaction():
     def date(self):
         con= sqlite3.connect(self.dbfile)
         cur = con.cursor()
-<<<<<<< HEAD
-        cur.execute("select year,sex,count(*) from names2K where year>2004 and year<2010 group by year, sex order by year")
-        toReturn = *cur.fetchall(), sep = "\n"
-        con.commit()
-        con.close()
-        return toReturn
-=======
-        cur.execute("SELECT rowid,item#,amount,category,date,description from transactions order by year")
+        cur.execute('''SELECT rowid,item#,amount,category,
+        date,description from transactions order by year''')
         tuples = cur.fetchall()
         con.commit()
         con.close()
         return to_cat_dict_list(tuples)
->>>>>>> 21e02899d019f53ff8926cc9f85080d239282101
 
     def month(self):
         con= sqlite3.connect(self.dbfile)
         cur = con.cursor()
-<<<<<<< HEAD
-        cur.execute("select year,sex,count(*) from names2K where year>2004 and year<2010 group by year, sex order by year")
-        toReturn = *cur.fetchall(), sep = "\n"
-        con.commit()
-        con.close()
-        return toReturn
-=======
-        cur.execute("SELECT rowid,item#,amount,category,date,description from transactions order by month")
+        cur.execute('''SELECT rowid,item#,amount,category,date,
+        description from transactions order by month''')
         tuples = cur.fetchall()
         con.commit()
         con.close()
         return to_cat_dict_list(tuples)
->>>>>>> 21e02899d019f53ff8926cc9f85080d239282101
 
     def year(self):
         con= sqlite3.connect(self.dbfile)
         cur = con.cursor()
-<<<<<<< HEAD
-        cur.execute("select year,sex,count(*) from names2K where year>2004 and year<2010 group by year, sex order by year")
-        toReturn = *cur.fetchall(), sep = "\n"
-        con.commit()
-        con.close()
-        return toReturn
-=======
-        cur.execute("SELECT rowid,item#,amount,category,date,description from transactions order by day")
+        cur.execute('''SELECT rowid,item#,amount,category,date,
+        description from transactions order by day''')
         tuples = cur.fetchall()
         con.commit()
         con.close()
         return to_cat_dict_list(tuples)
->>>>>>> 21e02899d019f53ff8926cc9f85080d239282101
 
     def category(self):
         con= sqlite3.connect(self.dbfile)
         cur = con.cursor()
-<<<<<<< HEAD
-        cur.execute("select year,sex,count(*) from names2K where year>2004 and year<2010 group by year, sex order by year")
-        toReturn = *cur.fetchall(), sep = "\n"
-        con.commit()
-        con.close()
-        return toReturn
-=======
-        cur.execute("SELECT rowid,item#,amount,category,date,description from transactions order by category")
+        cur.execute('''SELECT rowid,item#,amount,category,date,
+        description from transactions order by category''')
         tuples = cur.fetchall()
         con.commit()
         con.close()
         return to_cat_dict_list(tuples)
->>>>>>> 21e02899d019f53ff8926cc9f85080d239282101
