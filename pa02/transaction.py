bf0a48e7bdc2890d4e2f02f8baa86e91bf4a5fad
lmayancela <lmayancela08@gmail.com>
2022-03-24 22:25:26 -0400

    big fixes

--- pa02/transaction.py
+++ pa02/transaction.py
@@ -1,36 +1,61 @@
+'''
+docstring?
+'''
 import sqlite3
 
 def to_cat_dict(cat_tuple):
-    cat = {'item#':cat_tuple[1], 'amount':cat_tuple[2], 'category':cat_tuple[3], 'date':cat_tuple[4], 'description':cat_tuple[8]}
+    '''
+    put a docstring here
+    '''
+    cat = {'rowid':cat_tuple[0], 'itemnum':cat_tuple[1], 'amount':cat_tuple[2],
+    'category':cat_tuple[3], 'date':cat_tuple[4], 'description':cat_tuple[5]}
     return cat
 
 def to_cat_dict_list(cat_tuples):
+    '''
+    put a docstring here
+    '''
     return [to_cat_dict(cat) for cat in cat_tuples]
 
 class Transaction():
+    '''
+    put a docstring here
+    '''
 
-    def __init__(self,file_name):
+    def __init__(self,dbfile):
+        '''
+    put a docstring here
+    '''
         con= sqlite3.connect(dbfile)
         cur = con.cursor()
         cur.execute('''CREATE TABLE IF NOT EXISTS transactions
-                    (item# int,amount int,category text,date text,day text, month text, year text, description text)''')
+                    (itemnum int,amount float,category text,
+                    date text,day text, month text, year text,
+                    description text)''')
         con.commit()
         con.close()
         self.dbfile = dbfile
 
     def show(self):
+        '''
+    put a docstring here
+    '''
         con= sqlite3.connect(self.dbfile)
         cur = con.cursor()
-        cur.execute("SELECT rowid,item#,amount,category,date,description from transactions")
+        cur.execute("SELECT rowid,itemnum,amount,category,date,description from transactions")
         tuples = cur.fetchall()
         con.commit()
         con.close()
         return to_cat_dict_list(tuples)
 
     def add(self,item):
+        '''
+    put a docstring here
+    '''
         con= sqlite3.connect(self.dbfile)
         cur = con.cursor()
-        cur.execute("INSERT INTO transactions VALUES(?,?,?,?,?,?,?,?)",(item['item#'],item['amount'],item['category'],'date',item['day'],item['month'],item['year'],item['description']))
+        cur.execute("INSERT INTO transactions VALUES(?,?,?,?,?,?,?,?)",
+        (item['itemnum'],item['amount'],item['category'], item['date'],item['day'],item['month'],item['year'],item['description']))
         con.commit()
         cur.execute("SELECT last_insert_rowid()")
         last_rowid = cur.fetchone()
@@ -39,6 +64,9 @@ class Transaction():
         return last_rowid[0]
 
     def delete(self,rowid):
+        '''
+    put a docstring here
+    '''
         con= sqlite3.connect(self.dbfile)
         cur = con.cursor()
         cur.execute('''DELETE FROM transactions
@@ -48,36 +76,52 @@ class Transaction():
         con.close()
 
     def date(self):
+        '''
+    put a docstring here
+    '''
         con= sqlite3.connect(self.dbfile)
         cur = con.cursor()
-        cur.execute("SELECT rowid,item#,amount,category,date,description from transactions order by year")
+        cur.execute('''SELECT rowid,itemnum,amount,category,
+        date,description from transactions order by date''')
         tuples = cur.fetchall()
         con.commit()
         con.close()
         return to_cat_dict_list(tuples)
 
     def month(self):
+        '''
+    put a docstring here
+    '''
         con= sqlite3.connect(self.dbfile)
         cur = con.cursor()
-        cur.execute("SELECT rowid,item#,amount,category,date,description from transactions order by month")
+        cur.execute('''SELECT rowid,itemnum,amount,category,date,
+        description from transactions order by month''')
         tuples = cur.fetchall()
         con.commit()
         con.close()
         return to_cat_dict_list(tuples)
 
     def year(self):
+        '''
+    put a docstring here
+    '''
         con= sqlite3.connect(self.dbfile)
         cur = con.cursor()
-        cur.execute("SELECT rowid,item#,amount,category,date,description from transactions order by day")
+        cur.execute('''SELECT rowid,itemnum,amount,category,date,
+        description from transactions order by year''')
         tuples = cur.fetchall()
         con.commit()
         con.close()
         return to_cat_dict_list(tuples)
 
     def category(self):
+        '''
+    put a docstring here
+    '''
         con= sqlite3.connect(self.dbfile)
         cur = con.cursor()
-        cur.execute("SELECT rowid,item#,amount,category,date,description from transactions order by category")
+        cur.execute('''SELECT rowid,itemnum,amount,category,date,
+        description from transactions order by category''')
         tuples = cur.fetchall()
         con.commit()
         con.close()
