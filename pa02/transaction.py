807bad63a4c642eced23144903d705a1d523ceda
Jiangarang <55816618+Jiangarang@users.noreply.github.com>
2022-03-24 20:13:45 -0400

    replace the 'docstrings' with a description of the method

--- pa02/transaction.py
+++ pa02/transaction.py
@@ -1,16 +1,31 @@
+'''
+docstring?
+'''
 import sqlite3
 
 def to_cat_dict(cat_tuple):
+    '''
+    put a docstring here
+    '''
     cat = {'rowid':cat_tuple[0], 'item#':cat_tuple[1], 'amount':cat_tuple[2],
     'category':cat_tuple[3], 'date':cat_tuple[4], 'description':cat_tuple[5]}
     return cat
 
 def to_cat_dict_list(cat_tuples):
+    '''
+    put a docstring here
+    '''
     return [to_cat_dict(cat) for cat in cat_tuples]
 
 class Transaction():
+    '''
+    put a docstring here
+    '''
 
     def __init__(self,file_name):
+        '''
+    put a docstring here
+    '''
         con= sqlite3.connect(dbfile)
         cur = con.cursor()
         cur.execute('''CREATE TABLE IF NOT EXISTS transactions
@@ -22,6 +37,9 @@ class Transaction():
         self.dbfile = dbfile
 
     def show(self):
+        '''
+    put a docstring here
+    '''
         con= sqlite3.connect(self.dbfile)
         cur = con.cursor()
         cur.execute("SELECT rowid,item#,amount,category,date,description from transactions")
@@ -31,6 +49,9 @@ class Transaction():
         return to_cat_dict_list(tuples)
 
     def add(self,item):
+        '''
+    put a docstring here
+    '''
         con= sqlite3.connect(self.dbfile)
         cur = con.cursor()
         cur.execute("INSERT INTO transactions VALUES(?,?,?,?,?)",
@@ -44,6 +65,9 @@ class Transaction():
         return last_rowid[0]
 
     def delete(self,rowid):
+        '''
+    put a docstring here
+    '''
         con= sqlite3.connect(self.dbfile)
         cur = con.cursor()
         cur.execute('''DELETE FROM transactions
@@ -53,6 +77,9 @@ class Transaction():
         con.close()
 
     def date(self):
+        '''
+    put a docstring here
+    '''
         con= sqlite3.connect(self.dbfile)
         cur = con.cursor()
         cur.execute('''SELECT rowid,item#,amount,category,
@@ -63,6 +90,9 @@ class Transaction():
         return to_cat_dict_list(tuples)
 
     def month(self):
+        '''
+    put a docstring here
+    '''
         con= sqlite3.connect(self.dbfile)
         cur = con.cursor()
         cur.execute('''SELECT rowid,item#,amount,category,date,
@@ -73,6 +103,9 @@ class Transaction():
         return to_cat_dict_list(tuples)
 
     def year(self):
+        '''
+    put a docstring here
+    '''
         con= sqlite3.connect(self.dbfile)
         cur = con.cursor()
         cur.execute('''SELECT rowid,item#,amount,category,date,
@@ -83,6 +116,9 @@ class Transaction():
         return to_cat_dict_list(tuples)
 
     def category(self):
+        '''
+    put a docstring here
+    '''
         con= sqlite3.connect(self.dbfile)
         cur = con.cursor()
         cur.execute('''SELECT rowid,item#,amount,category,date,
